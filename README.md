01a812d966c359937d2536692284f67b5b9f6ea4
Audun Simonsen <mcaasi@ASI-PC>
2018-09-19 14:14:13 +0200

    Min renovasjon component and sensor platform

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # home-assistant-custom-components
 Custom components for Home Assistant (https://www.home-assistant.io/)
+
+Min Renovasjon:
+
+```
+min_renovasjon:
+  street_name: "Min gate"
+  street_code: "00000"
+  house_no: "0"
+  county_id: "0000"
+
+
+sensor:
+  - platform: min_renovasjon
+    fraction_id:
+      - 1
+      - 2
+      - 3
+      - 19
+  ```
+  
+1: Restavfall
+
+2: Papir
+
+3: Matavfall
+
+11: Metall
+
+19: Plast
+  
--- min_renovasjon/min_renovasjon.py
+++ min_renovasjon/min_renovasjon.py
@@ -2,30 +2,72 @@ import requests
 import json
 from datetime import date
 from datetime import datetime
+import logging
+import homeassistant.helpers.config_validation as cv
+import voluptuous as vol
+
+_LOGGER = logging.getLogger(__name__)
+
+DOMAIN = "min_renovasjon"
+DATA_MIN_RENOVASJON = "data_min_renovasjon"
+
+CONF_STREET_NAME = "street_name"
+CONF_STREET_CODE = "street_code"
+CONF_HOUSE_NO = "house_no"
+CONF_COUNTY_ID = "county_id"
+CONF_DATE_FORMAT = "date_format"
+DEFAULT_DATE_FORMAT = "%d/%m/%Y"
 
 CONST_KOMMUNE_NUMMER = "Kommunenr"
 CONST_APP_KEY = "RenovasjonAppKey"
 CONST_URL_FRAKSJONER = 'https://komteksky.norkart.no/komtek.renovasjonwebapi/api/fraksjoner'
 CONST_URL_TOMMEKALENDER = 'https://komteksky.norkart.no/komtek.renovasjonwebapi/api/tommekalender?' \
                           'gatenavn=[gatenavn]&gatekode=[gatekode]&husnr=[husnr]'
-
-KOMMUNE_NUMMER = "0712"
-APP_KEY = "AE13DEEC-804F-4615-A74E-B4FAC11F0A30"
-
 CONST_DATA_FILENAME = "min_renovasjon.dat"
 CONST_DATA_HEADER_COMMENT = '# Auto-generated file. Do not edit.'
+CONST_APP_KEY_VALUE = "AE13DEEC-804F-4615-A74E-B4FAC11F0A30"
+
+CONFIG_SCHEMA = vol.Schema({
+    DOMAIN: vol.Schema({
+        vol.Required(CONF_STREET_NAME): cv.string,
+        vol.Required(CONF_STREET_CODE): cv.string,
+        vol.Required(CONF_HOUSE_NO): cv.string,
+        vol.Required(CONF_COUNTY_ID): cv.string,
+        vol.Optional(CONF_DATE_FORMAT, default=DEFAULT_DATE_FORMAT): cv.string,
+    })
+}, extra=vol.ALLOW_EXTRA)
 
 
-# noinspection PyShadowingNames
+async def async_setup(hass, config):
+    """Set up the MinRenovasjon component."""
+    street_name = config[DOMAIN][CONF_STREET_NAME]
+    street_code = config[DOMAIN][CONF_STREET_CODE]
+    house_no = config[DOMAIN][CONF_HOUSE_NO]
+    county_id = config[DOMAIN][CONF_COUNTY_ID]
+    date_format = config[DOMAIN][CONF_DATE_FORMAT]
+
+    min_renovasjon = MinRenovasjon(street_name, street_code, house_no, county_id, date_format)
+    hass.data[DATA_MIN_RENOVASJON] = min_renovasjon
+
+    return True
+
+
 class MinRenovasjon:
-    def __init__(self, gatenavn, gatekode, husnr):
+    def __init__(self, gatenavn, gatekode, husnr, kommunenr, date_format):
         self.gatenavn = gatenavn
         self.gatekode = gatekode
         self.husnr = husnr
-        pass
+        self._kommunenr = kommunenr
+        self._date_format = date_format
+        self._kalender_list = self._get_calendar_list()
+
+    def refresh_calendar(self):
+        do_refresh = self._check_for_refresh_of_data(self._kalender_list)
+        if do_refresh:
+            self._kalender_list = self._get_calendar_list()
 
     def _get_tommekalender_from_web_api(self):
-        header = {CONST_KOMMUNE_NUMMER: KOMMUNE_NUMMER, CONST_APP_KEY: APP_KEY}
+        header = {CONST_KOMMUNE_NUMMER: self._kommunenr, CONST_APP_KEY: CONST_APP_KEY_VALUE}
 
         url = CONST_URL_TOMMEKALENDER
         url = url.replace('[gatenavn]', self.gatenavn)
@@ -37,12 +79,11 @@ class MinRenovasjon:
             data = response.text
             return data
         else:
-            print(response.status_code)
+            _LOGGER.error(response.status_code)
             return None
 
-    @staticmethod
-    def _get_fraksjoner_from_web_api():
-        header = {CONST_KOMMUNE_NUMMER: KOMMUNE_NUMMER, CONST_APP_KEY: APP_KEY}
+    def _get_fraksjoner_from_web_api(self):
+        header = {CONST_KOMMUNE_NUMMER: self._kommunenr, CONST_APP_KEY: CONST_APP_KEY_VALUE}
         url = CONST_URL_FRAKSJONER
 
         response = requests.get(url, headers=header)
@@ -50,38 +91,33 @@ class MinRenovasjon:
             data = response.text
             return data
         else:
-            print(response.status_code)
+            _LOGGER.error(response.status_code)
             return None
 
     @staticmethod
     def _read_from_file():
         try:
-            print("READING CONTENT FROM FILE")
+            _LOGGER.debug("Reading content from file")
 
             file = open(CONST_DATA_FILENAME)
-
             lines = file.readlines()
             tommekalender = lines[1]
             fraksjoner = lines[2]
-
             file.close()
 
             return tuple((tommekalender, fraksjoner))
-
         except FileNotFoundError:
-            print("FILE NOT FOUND")
+            _LOGGER.debug("File not found")
             return None
 
     @staticmethod
     def _write_to_file(tommekalender, fraksjoner):
-        print("WRITING CONTENT TO FILE")
+        _LOGGER.debug("Writing content to file")
 
         file = open(CONST_DATA_FILENAME, "w")
-
         file.write("{}\n".format(CONST_DATA_HEADER_COMMENT))
         file.write("{}\n".format(tommekalender))
         file.write("{}\n".format(fraksjoner))
-
         file.close()
 
     def _get_from_web_api(self):
@@ -93,14 +129,14 @@ class MinRenovasjon:
 
         return tommekalender, fraksjoner
 
-    def get_calendar_list(self, refresh=False):
+    def _get_calendar_list(self, refresh=False):
         data = None
 
         if not refresh:
             data = self._read_from_file()
 
         if refresh or data is None:
-            print("REFRESH OR NO DATA. FETCHING FROM WEB.")
+            _LOGGER.debug("Refresh or no data. Fetching from API.")
             tommekalender, fraksjoner = self._get_from_web_api()
         else:
             tommekalender, fraksjoner = data
@@ -109,13 +145,13 @@ class MinRenovasjon:
 
         check_for_refresh = False
         if not refresh:
-            check_for_refresh = self.check_for_refresh_of_data(kalender_list)
+            check_for_refresh = self._check_for_refresh_of_data(kalender_list)
 
         if check_for_refresh:
-            print("REFRESHING DATA...")
-            kalender_list = self.get_calendar_list(refresh=True)
+            _LOGGER.debug("Refreshing data...")
+            kalender_list = self._get_calendar_list(refresh=True)
 
-        print("RETURNING CALENDAR LIST")
+        _LOGGER.debug("Returning calendar list")
         return kalender_list
 
     @staticmethod
@@ -144,28 +180,27 @@ class MinRenovasjon:
         return kalender_list
 
     @staticmethod
-    def check_for_refresh_of_data(kalender_list):
-        print("CHECKING IF DATA NEEDS REFRESH")
+    def _check_for_refresh_of_data(kalender_list):
+        _LOGGER.debug("Checking if data needs refresh")
 
         for entry in kalender_list:
             _, _, _, tommedato_forste, tommedato_neste = entry
 
             if tommedato_forste.date() < date.today() or tommedato_neste.date() < date.today():
-                print("DATA NEEDS REFRESH")
+                _LOGGER.debug("Data need refresh")
                 return True
 
         return False
 
+    def get_calender_for_fraction(self, fraksjon_id):
+        for entry in self._kalender_list:
+            entry_fraksjon_id, _, _, _, _= entry
+            if fraksjon_id == entry_fraksjon_id:
+                return entry
 
-min_renovasjon = MinRenovasjon('Kalli%C3%A5sen', '11700', '1')
-kalender_list = min_renovasjon.get_calendar_list()
-refresh_needed = min_renovasjon.check_for_refresh_of_data(kalender_list)
+    @property
+    def calender_list(self):
+        return self._kalender_list
 
-print(refresh_needed)
-
-for fraksjon_id, fraksjon_navn, fraksjon_ikon, tommedato_forste, tommedato_neste in kalender_list:
-    print(fraksjon_id)
-    print(fraksjon_navn)
-    print(fraksjon_ikon)
-    print(tommedato_forste.strftime("%d-%m-%Y"))
-    print(tommedato_neste.strftime("%d-%m-%Y"))
+    def format_date(self, date):
+        return date.strftime(self._date_format)
