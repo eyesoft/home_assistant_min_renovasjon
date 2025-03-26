61261c428fb5e65df6b470d12c694eae4849595a
Audun Simonsen <audun.simonsen@gmail.com>
2025-03-26 11:05:58 +0100

    Added translation

--- custom_components/min_renovasjon/const.py
+++ custom_components/min_renovasjon/const.py
@@ -1,5 +1,4 @@
-VERSION = "2.6.0"
-
+VERSION = "2.6.1"
 NAME = "Min Renovasjon"
 ISSUE_URL = "https://github.com/eyesoft/home_assistant_min_renovasjon/issues"
 DOMAIN = "min_renovasjon"
@@ -20,7 +19,6 @@ KOMTEK_API_BASE_URL = "https://norkartrenovasjon.azurewebsites.net/proxyserver.a
 CONST_URL_FRAKSJONER = KOMTEK_API_BASE_URL + "?server=https://komteksky.norkart.no/MinRenovasjon.Api/api/fraksjoner"
 CONST_URL_TOMMEKALENDER = KOMTEK_API_BASE_URL + "?server=https://komteksky.norkart.no/MinRenovasjon.Api/api/tommekalender/?gatenavn=[gatenavn]&gatekode=[gatekode]&husnr=[husnr]&fraDato=[fra_dato]&dato=[til_dato]&api-version=2"
 NUM_MONTHS = 6
-
 STARTUP_MESSAGE = f"""
 -------------------------------------------------------------------
 {NAME}
